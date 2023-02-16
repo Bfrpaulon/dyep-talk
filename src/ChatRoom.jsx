f2ab354b7a1d92fbbfa0ef9f5d1bab6410e526ac
Bruno Paulon <brunopaulon@outlook.com.br>
2023-02-16 19:11:03 +0000

    update

--- src/ChatRoom.jsx
+++ src/ChatRoom.jsx
@@ -1,5 +1,52 @@
 import React, { useState, useEffect, useRef } from 'react';
 import socketIOClient from 'socket.io-client';
+import styled from 'styled-components';
+
+const ChatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  background-color: #fff;
+`;
+
+const MessageContainer = styled.div`
+  flex-grow: 1;
+  padding: 16px;
+  overflow-y: auto;
+`;
+
+const MessageItem = styled.div`
+  margin: 8px;
+  padding: 8px;
+  border-radius: 4px;
+  background-color: #f6f8fa;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+`;
+
+const ChatForm = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  border-top: 1px solid #f6f8fa;
+`;
+
+const ChatInput = styled.input`
+  flex-grow: 1;
+  margin-right: 8px;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #f6f8fa;
+`;
+
+const ChatButton = styled.button`
+  background-color: #0084ff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 8px 16px;
+  cursor: pointer;
+`;
 
 function ChatRoom() {
   const [messages, setMessages] = useState([]);
@@ -10,13 +57,13 @@ function ChatRoom() {
     socketRef.current = socketIOClient('http://localhost:3000');
 
     socketRef.current.on('chat message', (msg) => {
-      setMessages([...messages, msg]);
+      setMessages((messages) => [...messages, msg]);
     });
 
     return () => {
       socketRef.current.disconnect();
     };
-  }, [messages]);
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -27,21 +74,22 @@ function ChatRoom() {
   };
 
   return (
-    <div>
-      <div>
+    <ChatContainer>
+      <MessageContainer>
         {messages.map((message, index) => (
-          <div key={index}>{message}</div>
+          <MessageItem key={index}>{message}</MessageItem>
         ))}
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input
+      </MessageContainer>
+      <ChatForm onSubmit={handleSubmit}>
+        <ChatInput
           type="text"
+          placeholder="Digite sua mensagem"
           value={text}
           onChange={(event) => setText(event.target.value)}
         />
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
+        <ChatButton type="submit">Enviar</ChatButton>
+      </ChatForm>
+    </ChatContainer>
   );
 }
 
